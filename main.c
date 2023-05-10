7b59adf8d2dc188f61cbd4324df7d6ce2bee6f26
amaari <arish2k3@gmail.com>
2023-05-10 18:12:11 +0530

    add print_prompt function for REPL and InputBuffer struct

--- main.c
+++ main.c
@@ -2,3 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/types.h>
+
+void print_prompt() { printf("db > "); }
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
