8fb2526167f03e85b008f0190d5ecb8e8d4869e6
amaari <arish2k3@gmail.com>
2023-05-11 12:25:07 +0530

    add input_buffer checking if it's equal to .exit string then exit function which is close_input_buffer with memory freeing

--- main.c
+++ main.c
@@ -5,8 +5,19 @@
 
 void print_prompt() { printf("db > "); }
 
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
 int main(int argc, char* argv[]) {
     while (true) {
         print_prompt();
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized commmand '%s.\n", input_buffer->buffer);
+        }
     }
 }
