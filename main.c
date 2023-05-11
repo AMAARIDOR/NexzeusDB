f33b4768154bb3060b79d925800de5b803be8bf2
amaari <arish2k3@gmail.com>
2023-05-11 12:32:09 +0530

    add input buffer new_input_buffer function with malloc size of InputBuffer struct and setting each value to 0 because it's new

--- main.c
+++ main.c
@@ -12,14 +12,24 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+    return input_buffer;
+}
+
 void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
 }
 
 int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
+        read_input(input_buffer);
         if (strcmp(input_buffer->buffer, ".exit") == 0) {
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
