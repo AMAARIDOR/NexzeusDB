0972f8ca0be809ddcdf2f799f7800bdfa476aaab
Amaari <arish2k3@gmail.com>
2023-05-12 19:28:23 +0530

    change positions of struct Statement because of error

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
-typedef struct {
-    StatementType type;
-} Statement;
-
 typedef enum { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND } MetaCommandResult;
 typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
