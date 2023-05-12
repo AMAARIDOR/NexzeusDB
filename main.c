af1512053fafecc49c6223d95bd9ecae91051105
Amaari <arish2k3@gmail.com>
2023-05-12 19:24:50 +0530

    add PrepareResult enum for success and unrecognized statement values that are going to be used for a switch statement that will check if the statement is a prepare success or not, between that it is also going to check if the statement is insert or select for now. will add more commands later. then at last we will execute the statement with execute statement checking the values of the type of the statement whether or not its a insert or select.

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
-// typedef struct {
-//     StatementType type;
-// } Statement;
+typedef struct {
+    StatementType type;
+} Statement;
 
 typedef enum { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND } MetaCommandResult;
-// typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
-// typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -37,28 +37,28 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
     }
 }
 
-// PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
-//     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-//         statement->type = STATEMENT_INSERT;
-//         return PREPARE_SUCCESS;
-//     }
-//     if (strcmp(input_buffer->buffer, "select") == 0) {
-//         statement->type = STATEMENT_SELECT;
-//         return PREPARE_SUCCESS;
-//     }
-//     return PREPARE_UNRECOGNIZED_STATEMENT;
-// }
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
 
-// void execute_statement(Statement* statement) {
-//     switch (statement->type) {
-//         case (STATEMENT_INSERT):
-//             printf("This is where we would do an insert.\n");
-//             break;
-//         case (STATEMENT_SELECT):
-//             printf("This is where we would do a select.\n");
-//             break;
-//     }
-// }
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            printf("This is where we would do an insert.\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is where we would do a select.\n");
+            break;
+    }
+}
 
 void read_input(InputBuffer* input_buffer) {
     ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
@@ -90,16 +90,16 @@ int main(int argc, char* argv[]) {
                         continue;
                 }
             }
-            // Statement statement;
-            // switch (prepare_statement(input_buffer, &statement)) {
-            //     case (PREPARE_SUCCESS):
-            //         break;
-            //     case (PREPARE_UNRECOGNIZED_STATEMENT):
-            //         printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
-            //         continue;
-            // }
-            // execute_statement(&statement);
-            // printf("Executed.\n");
+            Statement statement;
+            switch (prepare_statement(input_buffer, &statement)) {
+                case (PREPARE_SUCCESS):
+                    break;
+                case (PREPARE_UNRECOGNIZED_STATEMENT):
+                    printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
+            execute_statement(&statement);
+            printf("Executed.\n");
         }
     }
 }
