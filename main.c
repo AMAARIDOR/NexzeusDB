3b9fa8a40ce3c816fb673f42ba3dfa36d3410c4d
Amaari <arish2k3@gmail.com>
2023-05-15 18:04:00 +0530

    add is args are less than 3 in insert, prepare syntax error

--- main.c
+++ main.c
@@ -59,6 +59,9 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
         int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", 
         &(statement->row_to_insert.id), statement->row_to_insert.username, 
         statement->row_to_insert.email);
+        if (args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
